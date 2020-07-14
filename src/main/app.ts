5f117953ce5bdb5f92e04697275bee67439a8e0f
Quintinity <vlad.marica1@gmail.com>
2020-07-13 21:27:41 -0300

    Initial commit

---
+++ src/main/app.ts
@@ -0,0 +1,32 @@
+import path from 'path';
+import { app, BrowserWindow, Menu, Tray } from 'electron';
+
+const TITLE = 'Twitch Desktop Notifier';
+const RES_PATH = '../../res';
+
+function getResource(resourceName: string) {
+  return path.join(app.getAppPath(), RES_PATH, path.normalize(resourceName));
+}
+
+let tray = null;
+let window = null;
+app.on('ready', () => {
+  tray = new Tray(getResource('fallback_icon.png'));
+  tray.setToolTip(TITLE);
+  tray.setTitle(TITLE);
+  tray.setContextMenu(Menu.buildFromTemplate([
+    { label: 'Exit', type: 'normal', click: () => console.log('exit') }
+  ]));
+
+  window = new BrowserWindow({
+    width: 800,
+    height: 600,
+    title: TITLE,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  window.loadFile(getResource('index.html'));
+  // window.removeMenu();
+});
